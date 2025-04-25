69e0a2867e8ed0710f31d36a74c2c958e4b5f601
JiuMeng <94657758+64958311@users.noreply.github.com>
2025-04-25 10:17:48 +0800

    Delete __init__.py

--- __init__.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-from app.api import users, pushs, logs, admin
-
-api_router = APIRouter()
-
-api_router.include_router(users.router, prefix="/users", tags=["users"])
-api_router.include_router(pushs.router, prefix="/pushs", tags=["pushs"])
-api_router.include_router(logs.router, prefix="/logs", tags=["logs"])
-api_router.include_router(admin.router, prefix="/admin", tags=["admin"])
